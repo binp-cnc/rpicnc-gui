5b8e79e34f1a64215f017ac0c6d90868f44be3ea
Alex <alexey.gerasev@gmail.com>
2018-01-10 16:41:44 +0700

    connection to host gui

--- start.py
+++ start.py
@@ -1,27 +1,109 @@
 import sys
 import zmq
 import time
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QToolTip
-from PyQt5.QtGui import QFont
+import json
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtCore import Qt
+import logging as log
+
+log.basicConfig(level=log.DEBUG, format="[%(levelname)s] %(message)s")
 
 
-class Window(QWidget):
-	def __init__(self, socket):
+context = zmq.Context()
+
+
+class Connect(QWidget):
+	def __init__(self):
 		super().__init__()
-		self.socket = socket
 
+		self.socket = None
+
+		self.label = QLabel("Address:")
+		self.addrinput = QLineEdit()
+		self.addrinput.setText("10.0.0.2:5556")
+
+		self.button = QPushButton()
+		#self.button.setToolTip("This is a <b>QPushButton</b> widget")
+
+		self.layout = QHBoxLayout()
+		self.layout.addWidget(self.label)
+		self.layout.addWidget(self.addrinput, 1)
+		self.layout.addWidget(self.button)
+		self.setLayout(self.layout)
+
+		self.setstatus(False)
+
+	def connect(self):
+		socket = context.socket(zmq.PAIR)
+		addr = self.addrinput.text()
+		if ":" not in addr:
+			addr += ":5556"
+		socket.connect("tcp://%s" % addr)
+		socket.send(json.dumps({"type": "init"}).encode("utf-8"))
+		if json.loads(socket.recv().decode("utf-8"))["status"] == "ok":
+			log.debug("connected to '%s'" % addr)
+			self.setstatus(True)
+		else:
+			log.error("cannot connect to '%s'" % addr)
+
+	def disconnect(self):
+		log.debug("disconnected")
+		self.setstatus(False)
+
+	def setstatus(self, connected):
+		try:
+			self.button.clicked.disconnect()
+		except TypeError:
+			pass
+		self.addrinput.setEnabled(not connected)
+
+		if connected:
+			self.button.setText("Disconnect")
+			self.button.clicked.connect(self.disconnect)
+		else:
+			self.button.setText("Connect")
+			self.button.clicked.connect(self.connect)
+
+class Pannel(QWidget):
+	def __init__(self, scene, view):
+		super().__init__()
+
+		self.view = view
+		self.scene = scene
+
+		self.connect = Connect()
+
+		self.layout = QVBoxLayout()
+		self.layout.setAlignment(Qt.AlignTop)
+		self.layout.addWidget(self.connect)
+		self.setLayout(self.layout)
+
+
+class Window(QMainWindow):
+	def __init__(self):
+		super().__init__()
 		QToolTip.setFont(QFont('SansSerif', 10))
 
-		self.button = QPushButton('Sync', self)
-		self.button.setToolTip('This is a <b>QPushButton</b> widget')
-		self.button.resize(self.button.sizeHint())
-		self.button.move(50, 50)
-		self.button.clicked.connect(lambda: self.sync())
+		self.scene = QGraphicsScene()
+		self.view = QGraphicsView()
+		self.view.setScene(self.scene)
 
-		self.resize(800, 600)
+		self.pannel = Pannel(self.scene, self.view)
+
+		self.layout = QHBoxLayout()
+		self.layout.addWidget(self.view, 2)
+		self.layout.addWidget(self.pannel, 1)
+
+		self.layoutwrapper = QWidget()
+		self.layoutwrapper.setLayout(self.layout)
+		self.setCentralWidget(self.layoutwrapper)
+
+		self.resize(900, 600)
 		self.setWindowTitle("RPi CNC")
 		self.show()
 
+	"""
 	def sync(self):
 		msg = b"client request"
 		socket.send(msg)
@@ -29,13 +111,8 @@ class Window(QWidget):
 
 		msg = socket.recv()
 		print("[recv] '%s'" % msg)
-
-
-context = zmq.Context()
-socket = context.socket(zmq.PAIR)
-socket.connect("tcp://10.0.0.2:5556")
-print("[info] connected")
+	"""
 
 app = QApplication(sys.argv)
-window = Window(socket)
+window = Window()
 sys.exit(app.exec_())
